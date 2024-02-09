277ad357f6f1fab6a14c8a85f30700e4ac4048d0
Jakub Mikulas <jakub@mikul.as>
2024-02-09 15:45:45 +0100

    remove history scopes

--- src/scopes.ts
+++ src/scopes.ts
@@ -1,11 +1,7 @@
 export default [
-  'channels:history',
   'channels:join',
   'chat:write',
   'commands',
   'files:write',
-  'groups:history',
-  'im:history',
   'im:write',
-  'mpim:history',
 ];
