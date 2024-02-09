00808fdb8530ccf4d93b63aa58731acda8bc9bee
Jakub Mikulas <jakub@mikul.as>
2024-02-09 15:45:46 +0100

    improve the failed/cancelled installation page

--- src/init/index.ts
+++ src/init/index.ts
@@ -18,6 +18,29 @@ const app = new App({
     // https://github.com/slackapi/bolt-js/issues/1355
     stateVerification: false,
     directInstall: true,
+    callbackOptions: {
+      failure: (error, _installOptions, req, res) => {
+        console.log('Failed installation', error);
+        res.statusCode = 200;
+        res.end(`
+        <html>
+        <head>
+        <style>
+        body {
+          padding: 10px 15px;
+          font-family: verdana;
+          text-align: center;
+        }
+        </style>
+        </head>
+        <body>
+        <h2><a href="https://mermaid-preview.com">Mermaid Preview</a> was not installed</h2>
+        <p>You can try to <a href="https://mermaid-preview.com">install Mermaid Preview</a> again!</p>
+        </body>
+        </html>
+        `);
+      },
+    },
   },
   scopes,
   customRoutes,
