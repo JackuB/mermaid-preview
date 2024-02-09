4894dcce0b8415acfdb53a0c28f45f60e274928d
Jakub Mikulas <jakub@mikul.as>
2024-02-09 15:45:45 +0100

    use fly.io static hosting

--- src/customRoutes.ts
+++ src/customRoutes.ts
@@ -2,16 +2,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { type CustomRoute } from '@slack/bolt';
 
-// I can't really get Bolt.js to serve static files, without replacing the whole OAuth/Express app
-// If this approach would be an issue, I could setup a CDN/reverse proxy just for the statics
 const indexHTML = fs.readFileSync(
   path.resolve(__dirname, '../public/index.html')
-);
-const policyTXT = fs.readFileSync(
-  path.resolve(__dirname, '../public/policy.txt')
-);
-const screenshotJPEG = fs.readFileSync(
-  path.resolve(__dirname, '../public/mermaid-for-slack-preview-screenshot.jpg')
 );
 
 const customRoutes: CustomRoute[] = [
@@ -21,22 +13,6 @@ const customRoutes: CustomRoute[] = [
     handler: (req, res) => {
       res.setHeader('Content-Type', 'text/html');
       res.end(indexHTML);
-    },
-  },
-  {
-    path: '/privacy',
-    method: ['GET'],
-    handler: (req, res) => {
-      res.setHeader('Content-Type', 'text/plain');
-      res.end(policyTXT);
-    },
-  },
-  {
-    path: '/mermaid-for-slack-preview-screenshot.jpg',
-    method: ['GET'],
-    handler: (req, res) => {
-      res.setHeader('Content-Type', 'image/jpeg');
-      res.end(screenshotJPEG);
     },
   },
 ];
